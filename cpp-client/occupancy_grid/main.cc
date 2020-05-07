1e37c7365010da7c9a7169ea9fee169baba9584a
Brad Null <brad@bradleynull.com>
2020-05-07 08:57:50 -0500

    Forgot to add occupancy grid app

---
+++ cpp-client/occupancy_grid/main.cc
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <thread>
+#include <future>
+
+#include "Simulator.h"
+
+std::vector<std::shared_ptr<Sensor>> create_sensors_for(const std::string& ip)
+{
+    // Configure the sensors we wish to use
+    std::vector<std::shared_ptr<Sensor>> sensors;
+    OccupancyGridConfig occ_config;
+    occ_config.server_ip = ip;
+    occ_config.listen_port = 8100;
+    occ_config.resolution = Resolution(512, 512);
+    occ_config.meters_per_pixel = 0.1;
+    sensors.push_back(std::make_shared<Sensor>(
+      std::make_unique<OccupancyGridConfig>(occ_config)));
+
+    ViewportCameraConfig vp_config;
+    vp_config.server_ip = ip;
+    vp_config.location.z = 200;
+    vp_config.resolution = Resolution(256,256);
+    Sensor(std::make_unique<ViewportCameraConfig>(vp_config)).configure();
+
+    // Send configurations to the simulator
+    std::cout << "***********ALL SENSOR's CONFIGS*******" << std::endl;
+    for (auto& sensor : sensors) {
+      std::cout << "Sensor:" << sensor->config->dump() << std::endl;
+      sensor->configure();
+    }
+    return sensors;
+}
+
+int main(int argc, char** argv) {
+  std::string server0_ip = "127.0.0.1";
+  int server_port = 8999;  // This has to be 8999 this simulator is listening
+                           // for connections on this port;
+  
+  // Read JSON files in cpp_client/config directory
+  Configuration config("cpp-client/parser_dev/simulator.json",
+                       "config/vehicle.json", "config/weather.json",
+                       "cpp-client/buffer_dev/scenario.json");
+  Simulator& sim0 = Simulator::getInstance(config, server0_ip, server_port);
+
+  if (!sim0.configure()) {
+    return -1;
+  }
+
+  // Setup and Connect Sensors
+  std::vector<std::shared_ptr<Sensor>> sensors = create_sensors_for(server0_ip);
+
+  // Step through scenario while reading sensor outputs
+  std::future<bool> stepTask;
+  /// initialize the vehicle, the first control command spawns the vehicle
+  sim0.send_command(ApiMessage(123, EgoControl_ID, true,
+                               {{"forward_amount", 0.0},
+                                {"right_amount", 0.0},
+                                {"brake_amount", 0.0},
+                                {"drive_mode", 1}}));
+  for (auto& sensor : sensors) {
+    sensor->sample();
+  }
+  bool bContinue = true;
+
+  while (true) {
+    std::cout << "Sampling Sensors" << std::endl;
+    ApiMessage sampleMessage(999, SampleSensorsCommand_ID, true, {});
+    for (auto& sensor : sensors) {
+      sensor->sampleInProgress.store(true,
+                                     std::memory_order::memory_order_relaxed);
+    }
+    sim0.send_command(sampleMessage);
+    bool samplingInProgress = true;
+    do {
+      samplingInProgress = false;
+      for (auto& sensor : sensors) {
+        if (sensor->sampleInProgress.load(
+                std::memory_order::memory_order_relaxed)) {
+          samplingInProgress = true;
+          break;
+        }
+      }
+    } while (samplingInProgress);
+  }
+
+  return 0;
+}
