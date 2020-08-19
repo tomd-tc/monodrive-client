114a809afb3ecc3e737e2d2759f8874612724db2
Pedro Larios <pedro.larios@gmail.com>
2020-08-19 11:46:17 -0500

    more message conversions

--- monodrive/ros/src/monodrive_msgs/include/MessageFactory.h
+++ monodrive/ros/src/monodrive_msgs/include/MessageFactory.h
@@ -16,7 +16,7 @@ namespace monodrive_msgs
     class MessageFactory 
     {
 
-        static geometry_msgs::Vector3 FromMonoDriveType(const Vec3f& vec) {
+        static geometry_msgs::Vector3 Vec3fToROSVector3(const Vec3f& vec) {
             geometry_msgs::Vector3 result;
             result.x = vec.x;
             result.y = vec.y;
@@ -24,7 +24,15 @@ namespace monodrive_msgs
             return result;
         }
 
-        static Vec3f ToMonoDriveType(const geometry_msgs::Vector3& vec) {
+        static geometry_msgs::Point Vec3fToROSPoint(const Vec3f& vec) {
+            geometry_msgs::Point result;
+            result.x = vec.x;
+            result.y = vec.y;
+            result.z = vec.z;
+            return result;
+        }
+
+        static Vec3f ROSVector3ToVec3f(const geometry_msgs::Vector3& vec) {
             Vec3f result;
             result.x = vec.x;
             result.y = vec.y;
@@ -32,82 +40,161 @@ namespace monodrive_msgs
             return result;
         }
 
+        static geometry_msgs::Quaternion QuatToROSQuaternion(const ::Quat& quat) {
+            geometry_msgs::Quaternion result;
+            result.x = quat.x;
+            result.y = quat.y;
+            result.z = quat.z;
+            result.w = quat.w;
+            return result;
+        }
 
-        static monodrive_msgs::Waypoint FromMonoDriveType(const ::Waypoint& waypoint) {
+        static ::Quat ROSQuaternionToQuat(const geometry_msgs::Quaternion& quat) {
+            ::Quat result;
+            result.x = quat.x;
+            result.y = quat.y;
+            result.z = quat.z;
+            result.w = quat.w;
+            return result;
+        }
+
+        static geometry_msgs::Transform TransformToROSTransform(const ::Transform& transform) {
+            geometry_msgs::Transform result;
+            result.translation = Vec3fToROSVector3(transform.position);
+            result.rotation = QuatToROSQuaternion(transform.orientation);
+            return result;
+        }
+
+        static ::Transform ROSTransformToTransform(const geometry_msgs::Transform& transform) {
+            ::Transform result;
+            result.position = ROSVector3ToVec3f(transform.translation);
+            result.orientation = ROSQuaternionToQuat(transform.rotation);
+            return result;
+        }
+
+        static geometry_msgs::PoseWithCovariance TransformToPose(const ::Transform& transform) {
+            geometry_msgs::PoseWithCovariance result;
+            result.pose.position = Vec3fToROSPoint(transform.position);
+            result.pose.orientation = QuatToROSQuaternion(transform.orientation);
+            return result;
+        }
+
+        static geometry_msgs::TwistWithCovariance Vec3ToTwist(const ::Vec3f& linear_velocity, const ::Vec3f& angular_velocity) {
+            geometry_msgs::TwistWithCovariance result;
+            result.twist.linear = Vec3fToROSVector3(linear_velocity);
+            result.twist.angular = Vec3fToROSVector3(angular_velocity);
+            return result;
+        }
+
+        static monodrive_msgs::Waypoint WaypointToROSWaypoint(const ::Waypoint& waypoint) {
             monodrive_msgs::Waypoint message;
+            message.lane_change = waypoint.lane_change;
+            message.distance = waypoint.distance;
+            message.location = Vec3fToROSVector3(waypoint.location);
+            message.rotation = Vec3fToROSVector3(waypoint.rotation);
             return message;
         }
 
-        static ::Waypoint ToMonoDriveType(const monodrive_msgs::Waypoint& message) {
+        static ::Waypoint ROSWaypointToWaypoint(const monodrive_msgs::Waypoint& message) {
             ::Waypoint waypoint;
-            waypoint.location = ToMonoDriveType(message.location);
-            waypoint.rotation = ToMonoDriveType(message.rotation);
+            waypoint.location = ROSVector3ToVec3f(message.location);
+            waypoint.rotation = ROSVector3ToVec3f(message.rotation);
             waypoint.distance = message.distance;
             waypoint.lane_change = message.lane_change;
             return waypoint;
         }
 
-        static monodrive_msgs::ActorLane FromMonoDriveType(const ::ActorLane& actorLane) {
+        static monodrive_msgs::ActorLane ActorLaneToROSActorLane(const ::ActorLane& actorLane) {
             monodrive_msgs::ActorLane result;
             result.road_id = actorLane.road_id;
             result.lane_id = actorLane.lane_id;
             for (auto& waypoint : actorLane.waypoints) {
-                result.waypoints.push_back(FromMonoDriveType(waypoint));
+                result.waypoints.push_back(WaypointToROSWaypoint(waypoint));
             }
             return result;
         }
 
-        static ::ActorLane ToMonoDriveType(const monodrive_msgs::ActorLane& actorLane) {
+        static ::ActorLane ROSActorLaneToActorLane(const monodrive_msgs::ActorLane& actorLane) {
             ::ActorLane result;
             result.road_id = actorLane.road_id;
             result.lane_id = actorLane.lane_id;
             for (auto& waypoint : actorLane.waypoints) {
-                result.waypoints.push_back(ToMonoDriveType(waypoint));
+                result.waypoints.push_back(ROSWaypointToWaypoint(waypoint));
             }
             return result;
         }
 
-        static monodrive_msgs::ActorWaypoints FromMonoDriveType(const ::ActorWaypoints& waypoint) {
+        static monodrive_msgs::ActorWaypoints ActorWaypointsToROSActorWaypoints(const ::ActorWaypoints& waypoint) {
             monodrive_msgs::ActorWaypoints result;
             result.actor_id = waypoint.actor_id;
             result.actor_road_id = waypoint.actor_road_id;
             result.actor_lane_id = waypoint.actor_lane_id;
-            result.actor_waypoint = FromMonoDriveType(waypoint.actor_waypoint);
+            result.actor_waypoint = WaypointToROSWaypoint(waypoint.actor_waypoint);
             
             for (auto& lane : waypoint.lanes) {
-                result.lanes.push_back(FromMonoDriveType(lane));
+                result.lanes.push_back(ActorLaneToROSActorLane(lane));
             }
             for (auto& lane : waypoint.left_lanes) {
-                result.left_lanes.push_back(FromMonoDriveType(lane));
+                result.left_lanes.push_back(ActorLaneToROSActorLane(lane));
             }
             for (auto& lane : waypoint.right_lanes) {
-                result.right_lanes.push_back(FromMonoDriveType(lane));
+                result.right_lanes.push_back(ActorLaneToROSActorLane(lane));
             }
             return result;
         }
 
-        static ::ActorWaypoints ToMonoDriveType(const monodrive_msgs::ActorWaypoints& waypoint) {
+        static ::ActorWaypoints ROSActorWaypointsToActorWaypoints(const monodrive_msgs::ActorWaypoints& waypoint) {
             ::ActorWaypoints result;
             result.actor_id = waypoint.actor_id;
             result.actor_road_id = waypoint.actor_road_id;
             result.actor_lane_id = waypoint.actor_lane_id;
-            result.actor_waypoint = ToMonoDriveType(waypoint.actor_waypoint);
+            result.actor_waypoint = ROSWaypointToWaypoint(waypoint.actor_waypoint);
             
             for (auto& lane : waypoint.lanes) {
-                result.lanes.push_back(ToMonoDriveType(lane));
+                result.lanes.push_back(ROSActorLaneToActorLane(lane));
             }
             for (auto& lane : waypoint.left_lanes) {
-                result.left_lanes.push_back(ToMonoDriveType(lane));
+                result.left_lanes.push_back(ROSActorLaneToActorLane(lane));
             }
             for (auto& lane : waypoint.right_lanes) {
-                result.right_lanes.push_back(ToMonoDriveType(lane));
+                result.right_lanes.push_back(ROSActorLaneToActorLane(lane));
             }
             return result;
         }
 
-        static sensor_msgs::VehiceState FromMonoDriveType(const ::VehicleState& vehicleState) {
-            sensor_msgs::VehiceState result;
-            result.name = vehicleState.name;
+        static monodrive_msgs::WheelState WheelStateToROSWheelState(const ::WheelState& wheelState) {
+            monodrive_msgs::WheelState result;
+            result.id = wheelState.id;
+            result.speed = wheelState.speed;
+            result.transform = TransformToROSTransform(wheelState.pose);
+            return result;
+        }
+
+        static monodrive_msgs::OOBB OOBBToROSOOBB(const ::OOBB& oobb) {
+            monodrive_msgs::OOBB result;
+            return result;
+        }
+
+        static monodrive_msgs::VehicleState VehicleStateToROSVehicleState(const ::VehicleState& vehicleState) {
+            monodrive_msgs::VehicleState result;
+            result.name = vehicleState.state.name;
+            result.pose = TransformToPose(vehicleState.state.odometry.pose);
+            result.twist = Vec3ToTwist(vehicleState.state.odometry.linear_velocity, vehicleState.state.odometry.angular_velocity);
+            result.tags = vehicleState.state.tags;
+
+            for (auto& wheel : vehicleState.wheels) {
+                result.wheels.push_back(WheelStateToROSWheelState(wheel));
+            }
+            
+            for (auto& oobb : vehicleState.state.oobbs) {
+                result.oobbs.push_back(OOBBToROSOOBB(oobb));
+            }
+            return result;
+        }
+
+        static ::VehicleState ROSVehicleStateToVehicleState(const monodrive_msgs::VehicleState& vehicleState) {
+            ::VehicleState result;
+            /*result.name = vehicleState.name;
             result.pose = FromMonoDriveType(vehicleState.pose);
             result.twist = FromMonoDriveType(vehicleState.twist);
             result.tags = vehicleState.tags;
@@ -118,24 +205,7 @@ namespace monodrive_msgs
             
             for (auto& oobb : oobbs) {
                 result.oobbs.push_back(FromMonoDriveType(oobb));
-            }
-            return result;
-        }
-
-        static ::VehiceState ToMonoDriveType(const monodrive_msgs::VehicleState& vehicleState) {
-            ::VehiceState result;
-            result.name = vehicleState.name;
-            result.pose = FromMonoDriveType(vehicleState.pose);
-            result.twist = FromMonoDriveType(vehicleState.twist);
-            result.tags = vehicleState.tags;
-
-            for (auto& wheel : vehicleState.wheels) {
-                result.wheels.push_back(FromMonoDriveType(wheel);)
-            }
-            
-            for (auto& oobb : oobbs) {
-                result.oobbs.push_back(FromMonoDriveType(oobb));
-            }
+            }*/
             return result;
         }
 
@@ -144,19 +214,19 @@ namespace monodrive_msgs
             message.header.stamp.sec = frame.timer / 19660800;
 //            message.orientation
             message.orientation_covariance = {0,0,0,0,0,0,0,0,0}; 
-            message.angular_velocity = FromMonoDriveType(frame.angular_velocity);
+            message.angular_velocity = Vec3fToROSVector3(frame.angular_velocity);
             message.angular_velocity_covariance = { 0,0,0,0,0,0,0,0,0 };
 
-            message.linear_acceleration = FromMonoDriveType(frame.acceleration);
+            message.linear_acceleration = Vec3fToROSVector3(frame.acceleration);
             message.linear_acceleration_covariance = { 0,0,0,0,0,0,0,0,0 }; 
             return message;
         }
 
         static ImuFrame ToMonoDriveFrame(const sensor_msgs::Imu& message) {
             ImuFrame frame;
-            frame.angular_velocity = ToMonoDriveType(message.angular_velocity);
+            frame.angular_velocity = ROSVector3ToVec3f(message.angular_velocity);
 
-            frame.acceleration = ToMonoDriveType(message.linear_acceleration);
+            frame.acceleration = ROSVector3ToVec3f(message.linear_acceleration);
 
             frame.timer = message.header.stamp.sec * 19660800;
             frame.checksum = 0;
@@ -170,7 +240,7 @@ namespace monodrive_msgs
             message.game_time = frame.game_time;
             message.sample_count = frame.sample_count;
             for (auto& actor_wp : frame.actor_waypoints) {
-                message.actor_waypoints.push_back(FromMonoDriveType(actor_wp));
+                message.actor_waypoints.push_back(ActorWaypointsToROSActorWaypoints(actor_wp));
             }
             return message;
         }
@@ -181,7 +251,7 @@ namespace monodrive_msgs
             frame.game_time = message.game_time;
             frame.sample_count = message.sample_count;
             for (auto& actor_wp : message.actor_waypoints) {
-                frame.actor_waypoints.push_back(ToMonoDriveType(actor_wp));
+                frame.actor_waypoints.push_back(ROSActorWaypointsToActorWaypoints(actor_wp));
             }
             return frame;
         }
@@ -192,8 +262,11 @@ namespace monodrive_msgs
             message.game_time = frame.game_time;
             message.sample_count = frame.sample_count;            
             for (auto& vs : frame.vehicles) {
-                message.vehicles.push_back(FromMonoDriveType(vs));
+                message.vehicles.push_back(VehicleStateToROSVehicleState(vs));
             }
+            //for (auto& objs : frame.objects) {
+            //    message.objects.push_back(FromMonoDriveType(vobjs));
+            //}
             return message;
         }
 
@@ -203,7 +276,7 @@ namespace monodrive_msgs
             frame.game_time = message.game_time;
             frame.sample_count = message.sample_count;
             for (auto& vs : message.vehicles) {
-                frame.vehicles.push_back(ToMonoDriveType(vs));
+                frame.vehicles.push_back(ROSVehicleStateToVehicleState(vs));
             }
             return frame;
         }
