41232b87ffcc4a89d1f798dd25c3c03b1912699a
Devin A. Conley <devinaconley@gmail.com>
2020-08-26 11:20:05 -0500

    strip out config property macro system

--- monodrive/core/src/UECompatability.h
+++ monodrive/core/src/UECompatability.h
@@ -11,26 +11,3 @@
 #ifndef UE_BUILD
 #define MONODRIVECORE_API
 #endif
-
-#ifdef UE_BUILD
-// sensor config class name (SCCN)
-#define SCCN(name) F ## name
-#define SENSOR_CONFIG USTRUCT(BlueprintType)
-#define SENSOR_CONFIG_GEN_BODY GENERATED_BODY()
-#define SENSOR_PROPERTY(category) UPROPERTY(Category = category, EditAnywhere, BlueprintReadWrite)
-#else
-#define SCCN(name) name
-#define SENSOR_CONFIG
-#define SENSOR_CONFIG_GEN_BODY
-#define SENSOR_PROPERTY(category)
-#endif
-
-#ifdef UE_BUILD
-#define STRING_CLS FString
-#define NAME_CLS FName
-#define VECTOR_CLS TArray
-#else
-#define STRING_CLS std::string
-#define NAME_CLS std::string
-#define VECTOR_CLS std::vector
-#endif
