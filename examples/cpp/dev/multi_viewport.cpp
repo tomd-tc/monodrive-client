cfe591a013cc8af03f9e3bb7a2f33fc7e5506c10
Devin A. Conley <devinaconley@gmail.com>
2020-09-10 04:12:51 -0500

    update multi viewport example for extra 360 camera

--- examples/cpp/dev/multi_viewport.cpp
+++ examples/cpp/dev/multi_viewport.cpp
@@ -44,12 +44,12 @@ int main(int argc, char** argv)
     vp1.server_port = sim.getServerPort();
     vp1.listen_port = 1;
     vp1.enable_streaming = false;
-    vp1.location.z = 125;
-    vp1.location.y = -25;
+    vp1.location.z = 150;
+    vp1.location.y = -50;
     vp1.rotation.yaw = -60;
     vp1.resolution = Resolution(512, 512);
     vp1.viewport.enable_viewport =  true;
-    vp1.viewport.window_offset = Resolution(0, 256);
+    vp1.viewport.window_offset = Resolution(0, 512);
     vp1.viewport.monitor_number = 0;
     Sensor(std::make_unique<CameraConfig>(vp1)).configure();
 
@@ -58,11 +58,11 @@ int main(int argc, char** argv)
     vp2.server_port = sim.getServerPort();
     vp2.listen_port = 2;
     vp2.enable_streaming = false;
-    vp2.location.z = 125;
+    vp2.location.z = 225;
     vp2.rotation.yaw = 0;
     vp2.resolution = Resolution(896, 512);
     vp2.viewport.enable_viewport =  true;
-    vp2.viewport.window_offset = Resolution(512, 256);
+    vp2.viewport.window_offset = Resolution(512, 512);
     vp2.viewport.monitor_number = 0;
     Sensor(std::make_unique<CameraConfig>(vp2)).configure();
 
@@ -71,29 +71,28 @@ int main(int argc, char** argv)
     vp3.server_port = sim.getServerPort();
     vp3.listen_port = 3;
     vp3.enable_streaming = false;
-    vp3.location.z = 125;
-    vp3.location.y = 25;
+    vp3.location.z = 150;
+    vp3.location.y = 50;
     vp3.rotation.yaw = 60;
     vp3.resolution = Resolution(512, 512);
     vp3.viewport.enable_viewport = true;
-    vp3.viewport.window_offset = Resolution(1408, 256);
+    vp3.viewport.window_offset = Resolution(1408, 512);
     vp3.viewport.monitor_number = 0;
     Sensor(std::make_unique<CameraConfig>(vp3)).configure();
 
-    // configure fisheye camera as viewport
-    FisheyeCameraConfig vp4;
+    // configure 360 camera as viewport
+    Camera360Config vp4;
     vp4.server_ip = sim.getServerIp();
     vp4.server_port = sim.getServerPort();
     vp4.listen_port = 4;
     vp4.enable_streaming = false;
     vp4.location.z = 225;
-    vp4.resolution = Resolution(512, 512);
+    vp4.resolution = Resolution(1024, 512);
     vp4.face_size = 512;
-    vp4.fov = 180.f;
     vp4.viewport.enable_viewport = true;
-    vp4.viewport.window_offset = Resolution(512, 128);
+    vp4.viewport.window_offset = Resolution(448, 0);
     vp4.viewport.monitor_number = 0;
-    Sensor(std::make_unique<FisheyeCameraConfig>(vp4)).configure();
+    Sensor(std::make_unique<Camera360Config>(vp4)).configure();
 
     // send control command
     while (true)
