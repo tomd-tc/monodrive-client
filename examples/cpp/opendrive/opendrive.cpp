8a6ab5b3078f0a16626ec3b028259d19d9ee2619
stephendb <stephenop@gmail.com>
2020-11-04 22:51:55 -0600

    confused about ordering

--- examples/cpp/opendrive/opendrive.cpp
+++ examples/cpp/opendrive/opendrive.cpp
@@ -13,20 +13,14 @@ int main(){
     auto map = opendrive::OpenDriveParser::Load(openDriveFileString);
     auto topo = map->GenerateTopology();
     int count = 0;
+    std::cout << "TOPO: " << std::endl;
     for(auto& points : topo){
-        // if(points.first.road_id == 0){
-            std::cout << points.first.road_id << " " <<  points.first.lane_id << " " << points.first.section_id << " " << points.first.s;
-            std::cout << " :Point " << count++ << std::endl;
-            std::cout << points.second.road_id << " " <<  points.second.lane_id << " " << points.second.section_id << " " << points.second.s << std::endl;
-        // }
+        std::cout << points.first.road_id << " " <<  points.first.lane_id << " " << points.first.section_id << " " << points.first.s;
+        std::cout << " :Point " << count++ << std::endl;
+        std::cout << points.second.road_id << " " <<  points.second.lane_id << " " << points.second.section_id << " " << points.second.s << std::endl;
     }
     carla::geom::Vector3D location(0,0,0);
     auto wayPoint = map->GetClosestWaypointOnRoad(location);
-    auto nextWaypoints = map->GetSuccessors(wayPoint.get());
-    std::cout << " NEXT " << std::endl;
-    for(auto& next : nextWaypoints){
-        std::cout << next.s << " " << next.road_id << " " << next.lane_id << std::endl;
-    }
 
 	auto getEndWaypoint = [&map](carla::road::element::Waypoint inWaypoint) {
 		if (inWaypoint.lane_id < 0) {
@@ -46,8 +40,31 @@ int main(){
 			};
 		}
 	};
+    std::cout << "p0-0: " << std::endl;
+    std::cout << wayPoint->s << " " << wayPoint->road_id << " " << wayPoint->lane_id << std::endl;
+
     auto endPoint = getEndWaypoint(wayPoint.get());
+    std::cout << "p0-1: " << std::endl;
     std::cout << endPoint.s << " " << endPoint.road_id << " " << endPoint.lane_id << std::endl;
+
+    std::cout << "p1-0: " << std::endl;
+    auto nextWaypoints = map->GetSuccessors(wayPoint.get());
+    for(auto& next : nextWaypoints){
+        std::cout << next.s << " " << next.road_id << " " << next.lane_id << std::endl;
+    }
+    auto nextEnd = getEndWaypoint(nextWaypoints[0]);
+    std::cout << "p1-1: " << std::endl;
+    std::cout << nextEnd.s << " " << nextEnd.road_id << " " << nextEnd.lane_id << std::endl;
+
+    std::cout << "p2-0: " << std::endl;
+    nextWaypoints = map->GetSuccessors(nextEnd);
+    for(auto& next : nextWaypoints){
+        std::cout << next.s << " " << next.road_id << " " << next.lane_id << std::endl;
+    }
+    nextEnd = getEndWaypoint(nextWaypoints[0]);
+    std::cout << "p2-1: " << std::endl;
+    std::cout << nextEnd.s << " " << nextEnd.road_id << " " << nextEnd.lane_id << std::endl;
+
 
 
 
